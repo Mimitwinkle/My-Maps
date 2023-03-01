c22adbf1783410267b94b8e1a252d9cfee2760f0
Kelly <kellygreentea32@gmail.com>
2023-03-01 17:09:26 -0500

    Added RecyclerView for maps

--- app/src/main/java/io/mimitwinkle/mymaps/MapsAdapter.kt
+++ app/src/main/java/io/mimitwinkle/mymaps/MapsAdapter.kt
@@ -2,11 +2,13 @@ package io.mimitwinkle.mymaps
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import io.mimitwinkle.mymaps.models.UserMap
 
-class MapsAdapter(val context: Context, val userMaps: List<UserMap>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class MapsAdapter(val context: Context, val userMaps: List<UserMap>) : RecyclerView.Adapter<MapsAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(context).inflate(android.R.layout.simple_list_item_1, parent, false)
@@ -15,9 +17,11 @@ class MapsAdapter(val context: Context, val userMaps: List<UserMap>) : RecyclerV
 
     override fun getItemCount() = userMaps.size
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val userMap = userMaps[position]
+        val textViewTitle = holder.itemView.findViewById<TextView>(android.R.id.text1)
+        textViewTitle.text = userMap.title
     }
 
-    class ViewHolder : RecyclerView.ViewHolder(itemView)
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 }
